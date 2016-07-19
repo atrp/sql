cce285d664af63f87e9d2689af8cfe10fdf7c527
Alex <alex@greenbit.ro>
2016-07-19 18:27:10 +0300

    Update show_worked_accouts_by_desk.sql

--- show_worked_accouts_by_desk.sql
+++ show_worked_accouts_by_desk.sql
@@ -1,2 +1,2 @@
-select * from CDS.DAT where DAT_USER_ID = 'AMO' AND DAT_TRX_DATE_O = '2016-07-19'
-AND DAT_TYPE IN ('S', 'C', 'H')
+/* show what accounts were worked by agent desk */
+select * from CDS.DAT where DAT_USER_ID = 'AMO' AND DAT_TRX_DATE_O = '2016-07-19' AND DAT_TYPE IN ('S', 'C', 'H')
